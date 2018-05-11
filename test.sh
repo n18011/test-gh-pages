74c39adb9acaa5590b32d68a0ba380574a575fa3
masahiro kina <n18011@std.it-college.ac.jp>
2018-05-11 07:42:20 +0000

    Revert "Revert "２０日より後の日付の処理を追加""
    This reverts commit 98dd4b11d4849647f472e8544c1b00a21990006b.

--- test.sh
+++ test.sh
@@ -8,6 +8,8 @@ if [ $today -eq 20 ]; then
   echo "今日は２０日です。月次〆処理を開始してください"
 elif [ $today -lt 20 ]; then
   echo "今日は２０日より前の日にちです."
+elif [ $today -lt 20 ]; then
+  echo "今日は２０日より後の日にちです."
 else
   echo "今日は２０日ではありません。"
 fi
